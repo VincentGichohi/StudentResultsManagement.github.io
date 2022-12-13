74050501cceea52f56628c0c3be14699adecd027
VincentGichohi <vincentegichohi@gmail.com>
2022-12-13 23:09:19 +0300

    add projects and apps

---
+++ results_management_system/results/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from students.models import Student
+from subjects.models import StudentClass
+from django.urls import reverse
+from django.db.models import JSONField
+
+
+class DeclareResult(models.Model):
+    select_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    select_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    marks = JSONField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse('results:declare_result')
+
+    def __str__(self):
+        return "%s Section-%s" % (self.select_class.class_name, self.select_class.section)
